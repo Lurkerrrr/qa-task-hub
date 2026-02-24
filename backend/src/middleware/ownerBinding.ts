c1e35de4bc004bb765e038c301b4043b5087b3de
debugger <meganesh2005@gmail.com>
2026-02-24 22:20:49 +0100

    refactor: replace generic AppError with ForbiddenError

--- backend/src/middleware/ownerBinding.ts
+++ backend/src/middleware/ownerBinding.ts
@@ -1,13 +1,13 @@
 import { Response, NextFunction } from 'express';
 import { AuthRequest } from '../interfaces';
-import { AppError } from '../utils/AppError';
+import { ForbiddenError } from '../utils/AppError';
 
 export const ownerBinding = (req: AuthRequest, res: Response, next: NextFunction): void => {
     const payloadName = req.userName;
     const requestName = req.body.assignee;
 
     if (requestName && payloadName !== requestName) {
-        return next(new AppError('Forbidden: You cannot perform actions on behalf of another user', 403));
+        return next(new ForbiddenError('You cannot perform actions on behalf of another user'));
     }
 
     next();
